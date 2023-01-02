1b97f13a9bf71ced1466d89415ff9f3d282d32b9
AlexeyG1991 <mumacil007@gmail.com>
2023-01-02 20:58:49 +0200

    freelancehunt

--- js/script.js
+++ js/script.js
@@ -5,14 +5,15 @@ let ua = {
     age: 'Повних років: 31',
     location: 'Місце проживання: м. Харків',
     experience: 'Досвід роботи',
-    experience_text: 'До 2020 року працював у виробничій сфері. Вирішив змінити діяльність, захопився версткою. Брав участь у розробці сайту.',
-    experience_text_2: 'Я працював у невеликій компанії (10 осіб) до її закриття. Брав активну участь у розробці трьох проектів як верстальник.',
+    // experience_text: 'До 2020 року працював у виробничій сфері. Вирішив змінити діяльність, захопився версткою. Брав участь у розробці сайту.',
+    experience_text: 'У 2020 році змінив свою діяльність, перейшов в IT сектор, а саме захопився HTML та CSS версткою. Брав участь у розробці сайтів (фріланс).',
+    experience_text_2: 'Працював у невеликій компанії, до 10 осіб. Брав активну участь у розробці різних проектів та завдань як front end.',
     personal_qualities: 'Особисті якості',
-    personal_qualities_text: 'Шанувальник безперервної освіти для актуалізації знань. Готовий присвятити себе роботі. <br>Прагнення до професіоналізму, багатозадачність, стресостійкість, уміння працювати з конфіденційною інформацією, відповідальність, комунікабельність, організованість, уважність до деталей, аналітичне мислення.',
+    personal_qualities_text: 'Шанувальник безперервної самоосвіти для актуалізації знань. Повністю концентруюся на роботі. Для мене робота — це не тільки те, що приносить гроші, але й те, що може принести мені моральне задоволення. <br>Прагнення до професіоналізму, багатозадачність, стресостійкість, уміння працювати з конфіденційною інформацією, відповідальність, комунікабельність, організованість, уважність до деталей, аналітичне мислення.',
     examples: 'Приклади робіт',
     // --------------------------------------
     skills: 'Навички',
-    skills_one: 'Адаптивна верстка.',
+    skills_one: 'Адаптивна верстка. Методологія БЕМ.',
     skills_two: 'Охайний, читаємий, продуманий код.',
     education: 'Освіта',
     education_info: 'ХДУХТ (2009-2014) <br>Спеціальність: економіка підприємства (спеціаліст)',
@@ -21,6 +22,31 @@ let ua = {
     lаnguage_ru: 'Російська<span></span>Вільно',
     lаnguage_en: 'Англійська<span></span>Ниже среднего',
     contact: 'Контакти',
+};
+
+let eng = {
+    name_l: 'G<span>|</span>O',
+    name: 'Grinchenko Oleksiy',
+    name_d: 'Front-end developer',
+    age: 'Age: 31 y.o.',
+    location: 'Location: Kharkov',
+    experience: 'Work experience',
+    experience_text: 'I changed my activities on the IT sector, interested in HTML and CSS layout in 2020. Participated in website development (freelance).',
+    experience_text_2: 'Worked in a small company, up to 10 people. Брав активну участь у розробці різних проектів та завдань як front end.',
+    personal_qualities: 'Soft skills',
+    personal_qualities_text: 'I constantly education to gain more knowledge. I fully concentrate on work. For me, work is not only a way to earn money, but it is also something that can give you moral pleasure, pride in what you do. <br>The pursuance of professionalism, multitasking, stress resistance, ability to work with confidential information, responsibility, communication skills, organised nature, attentive to details, analytical thinking.',
+    examples: 'Work examples',
+    // --------------------------------------
+    skills: 'Hard skills',
+    skills_one: 'Adaptive layout. BEM methodology.',
+    skills_two: 'Neat, readable, well thought-out code.',
+    education: 'Education',
+    education_info: 'KSUFTT (2009-2014) <br>Specialty: enterprise economics (specialist)',
+    lаnguage: 'Lаnguages',
+    lаnguage_ua: 'Ukrainian<span></span>Native',
+    lаnguage_ru: 'Russian<span></span>Fluently',
+    lаnguage_en: 'English<span></span>Pre-Intermediate',
+    contact: 'Contact',
 };
 
 // let ru = {
@@ -48,33 +74,8 @@ let ua = {
 //     contact: 'Контакты',
 // };
 
-let eng = {
-    name_l: 'H<span>|</span>A',
-    name: 'Hrinchenko Alexey',
-    name_d: 'Front-end developer',
-    age: 'Age: 31 y.o.',
-    location: 'Location: Kharkov',
-    experience: 'Work experience',
-    experience_text: 'I decided to change my activity since 2020 and got carried away with front-end development. I have taken part in some freelance-projects.',
-    experience_text_2: 'I worked for a small company (10 people) until it closed. I took one of the leading part in development of three projects as a layout designer.',
-    personal_qualities: 'Soft skills',
-    personal_qualities_text: 'I constantly education to gain more knowledge. I am ready to devote myself to work. For me, work is not only a way to earn money, but it is also something that can give you moral pleasure, pride in what you do. <br>The pursuance of professionalism, multitasking, stress resistance, ability to work with confidential information, responsibility, communication skills, organised nature, attentive to details, analytical thinking.',
-    examples: 'Work examples',
-    // --------------------------------------
-    skills: 'Hard skills',
-    skills_one: 'Adaptive layout.',
-    skills_two: 'Neat, readable, well thought-out code.',
-    education: 'Education',
-    education_info: 'KSUFTT (2009-2014) <br>Specialty: enterprise economics (specialist)',
-    lаnguage: 'Lаnguages',
-    lаnguage_ua: 'Ukrainian<span></span>Native',
-    lаnguage_ru: 'Russian<span></span>Fluently',
-    lаnguage_en: 'English<span></span>Pre-Intermediate',
-    contact: 'Contact',
-};
-
 function changeLagnuage() {
-    let language = lang.checked ? ua : eng;
+    let language = lang.checked ? eng : ua;
     document.querySelectorAll('[text]').forEach(el => {
         el.innerHTML = language[el.getAttribute('text')];
     })
